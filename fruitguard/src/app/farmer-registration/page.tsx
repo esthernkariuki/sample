1bdab69a2c4f388a39e04fb3de658b4f0a4f0c73
esther <esthernnyamburaa@gmail.com>
2025-10-15 08:19:14 +0300

    initial commit

--- fruitguard/src/app/farmer-registration/page.tsx
+++ fruitguard/src/app/farmer-registration/page.tsx
@@ -1,14 +1,13 @@
 "use client"
 import FarmersPage from "./components/FarmersTable"
 import AgrovetLayout from "../sharedComponents/AgrovetLayout"
-import MqttSubscriber from "../hivemq/page"
 
 export default function Register(){
     return(
         <AgrovetLayout>
             <div>
             <FarmersPage/>
-             <MqttSubscriber/>
+         
 
             
             </div>
